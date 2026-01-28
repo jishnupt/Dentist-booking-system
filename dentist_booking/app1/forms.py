c72062e1dda615ae3e42c1921845568f0e918f9b
jishnu <jishnuthilakp@gmail.com>
2026-01-28 20:56:40 +0530

    appoinment notification added for dr dashboard

--- dentist_booking/app1/forms.py
+++ dentist_booking/app1/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CustomUser
+from .models import CustomUser,Doctor_profile,Book_slot
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 
@@ -8,3 +8,27 @@ class UserRegForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username','email']
+
+class Dr_ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Doctor_profile
+        fields = '__all__'
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Book_slot
+        fields = [ 'booking_date', 'note']
+        widgets = {
+            'booking_date': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'min': timezone.now().date()
+                }
+            )
+        }
+
+    def clean_booking_date(self):
+        date = self.cleaned_data['booking_date']
+        if date < timezone.now().date():
+            raise forms.ValidationError("You cannot select a past date")
+        return date
--- dentist_booking/app1/urls.py
+++ dentist_booking/app1/urls.py
@@ -6,6 +6,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',homepage,name='homepage'),
     path('services_page',services_page,name='services_page'),
-    path('RegisterUser',RegisterUser,name='RegisterUser')
+    path('RegisterUser',RegisterUser,name='RegisterUser'),
+    path('AdminRegister',AdminRegister,name='AdminRegister'),
+    path('DoctorRegister',DoctorRegister),
+    path('Login_page',Login_page,name='Login_page'),
+    path('admin_dashbord',admin_dashbord),
+    path('doctor_dashbord',doctor_dashbord),
+    path('patient_dashbord',patient_dashbord,name='patient'),
+    path('contact_page',contact_page,name='contact'),
+    path('Dr_profile',Dr_profile),
+    path('Logout_page',Logout_page,name='logout'),
+    path('booking_slots/<int:did>',booking_slots,name='booking'),
+    path('Dr_notification',Dr_notification,name='Dr_notifi')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
